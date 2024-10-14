2a76064246317860f1d609acaf08eb3500edbea0
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2024-10-14 11:36:20 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/site/{any}', function () {
+    return file_get_contents(public_path('site/index.html'));
+})->where('any', '.*');
